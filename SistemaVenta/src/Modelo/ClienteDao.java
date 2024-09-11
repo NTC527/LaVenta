81bf3f67a002dd64a09bb206f634f436dcb85319
NTC527 <thiago.calderon527@gmail.com>
2024-09-10 22:40:44 -0400

    Add files via upload

--- SistemaVenta/src/Modelo/ClienteDao.java
+++ SistemaVenta/src/Modelo/ClienteDao.java
@@ -41,4 +41,23 @@ public class ClienteDao {
         }
         return ListaCl;
     }
+    
+    public boolean EliminarCliente(int id){
+        String sql = "DELETE FROM clientes WHERE id = ?";_
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.execute();
+            return true;
+        } catch (SQLException e) {
+            System.out.print(e.toString());
+            return false;
+        }finally{
+            try{
+                con.close();
+            }catch (SQLException ex) {
+                System.out.println(ex.toString());
+            }
+        }
+    }
 }
